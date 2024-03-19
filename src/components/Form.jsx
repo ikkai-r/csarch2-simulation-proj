8b1ebec3a807782d7bf97847d741c2a736de7c79
ikkai-r <rm.sales92@gmail.com>
2024-03-19 08:31:53 +0800

    feat: clear input & one dot implemented ; todo: make input to 0-1 only

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,24 +1,41 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Button from './Button';
-import { binaryValidationSchema } from '@/utils/validateBinary'
 
 const Form = () => {
     const [selectedOption, setSelectedOption] = useState("B"); 
-    const [binaryInput, setBinaryInput] = useState("");
-    const [binaryInputError, setBinaryInputError] = useState("");
+    const [numb, setNumb] = useState("");
+    const [dec, setDec] = useState("");
+    const [clearInput, setClearInput] = useState(false);
 
     const handleDropdownChange = (event) => {
         setSelectedOption(event.target.value);
+        setClearInput(true); 
     };
 
-    const handleBinaryInputChange = (event) => {
-        const inputValue = event.target.value;
-        setBinaryInput(inputValue);
+    const handleInputChange = (event) => {
+        let inputValue = event.target.value;
+        if (selectedOption === "B") {
+            let sanitizedInput = inputValue.replace(/[^0-1]/g, '').trim();
 
-        binaryValidationSchema.isValid({ binaryInput }).then(valid => {
-            setBinaryInputError(valid ? "" : "Invalid input. Only binary digits (0, 1) and dot (.) are allowed.");
-        });
+            const dotIndex = sanitizedInput.indexOf('.');
+            if (dotIndex !== -1) {
+                sanitizedInput = sanitizedInput.slice(0, dotIndex + 1) + sanitizedInput.slice(dotIndex + 1).replace('.', '');
+            }
+            console.log('here')
+
+            setNumb(inputValue);
+        } else {
+            setDec(inputValue);
+        }
     };
+
+    useEffect(() => {
+        if (clearInput) {
+            setNumb(""); 
+            setDec("");
+            setClearInput(false);
+        }
+    }, [selectedOption]); 
 
     return (
         <section>
@@ -26,16 +43,28 @@ const Form = () => {
             <form className="w-full md:px-10">
              <div className="grid md:grid-cols-4 md:gap-5 items-center justify-center">
                 <div className="mb-5 col-span-2">
-                    <label className="block mb-2 text-sm font-medium text-neutral-100">{selectedOption == "B" ? "Binary Mantissa" : "Decimal"}</label>
-                    <input 
-                           type="number" 
-                           id="mantissa-input" 
-                           className="block w-full p-2.5 text-neutral-100 border border-neutral-200 rounded-lg bg-neutral-400 text-sm focus:ring-blue-500 focus:border-blue-500"
-                           value={selectedOption == "B" ? binaryInput : ''}
-                           onChange={selectedOption == "B" ? handleBinaryInputChange : null}
-                    />
+                <label className="block mb-2 text-sm font-medium text-neutral-100">{selectedOption == "B" ? "Binary Mantissa" : "Decimal"}</label>
 
-                        {binaryInputError && <p className="text-red-500 text-sm mt-1">{binaryInputError}</p>}
+                    { selectedOption === "B" ? 
+                         <input 
+                                type="number" 
+                                id="mantissa-input" 
+                                className="block w-full p-2.5 text-neutral-100 border border-neutral-200 rounded-lg bg-neutral-400 text-sm focus:ring-blue-500 focus:border-blue-500"
+                                onChange={handleInputChange}
+                                min="0"
+                                max="1"
+                                value={numb}
+                                  />
+                    :
+                            <input 
+                                type="number" 
+                                id="mantissa-input" 
+                                className="block w-full p-2.5 text-neutral-100 border border-neutral-200 rounded-lg bg-neutral-400 text-sm focus:ring-blue-500 focus:border-blue-500"
+                                onChange={handleInputChange}
+                                value={dec}
+                            />
+                    }
+                    
                     
                 </div>
 
@@ -47,7 +76,7 @@ const Form = () => {
                 
                <div className="mb-5 col-span-2 md:col-span-1">
 
-               <label for="format" className="block mb-2 text-sm font-medium text-neutral-100">Format Option</label>
+               <label className="block mb-2 text-sm font-medium text-neutral-100">Format Option</label>
                     <select value={selectedOption} onChange={handleDropdownChange} id="format" className="bg-neutral-400 border  border-neutral-200 text-neutral-100 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
                         <option value="B">Binary</option>
                         <option value="D">Decimal</option>
--- src/utils/validateBinary.js
+++
@@ -1,5 +0,0 @@
-import * as Yup from 'yup';
-
-export const binaryValidationSchema = Yup.object().shape({
-    binaryInput: Yup.string().matches(/^[01]*\.?[01]*$/, 'Invalid input. Only binary digits (0, 1) and dot (.) are allowed.'),
-});
